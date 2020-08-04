aa740fe4228e6f95b395bee7b950fb1122a9527b
Yoo <sangduk.yoo@hpe.com>
2020-08-04 19:16:05 +0900

    modify yml1

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
     var dialog = document.querySelector('dialog');
+    
     dialogPolyfill.registerDialog(dialog);
+
     $(document).on('click', '.show-modal', function(){
         dialog.showModal();
     });
+
     dialog.querySelector('.close').addEventListener('click', function() {
         dialog.close();
     });
+
     dialog.querySelector('.print').addEventListener('click', function() {
         print();
     });
